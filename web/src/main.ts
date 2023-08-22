c4f190a90dfb9982c8b1afeeb9cb850b80c1f124
Yasser Greyeb <ygreyeb@gmail.com>
2023-08-22 17:16:28 -0500

    Add shoelace

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,2 +1,8 @@
 import 'htmx.org'
 import './style.css'
+import {setBasePath} from '@shoelace-style/shoelace/dist/utilities/base-path.js'
+import '@shoelace-style/shoelace/dist/themes/light.css'
+import '@shoelace-style/shoelace/dist/components/format-date/format-date.js'
+import '@shoelace-style/shoelace/dist/components/spinner/spinner.js'
+
+setBasePath('/shoelace')
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,17 @@
-import {defineConfig} from "vite";
+import {defineConfig} from "vite"
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 export default defineConfig({
     build: {
         outDir: "../src/main/resources/static",
         emptyOutDir: true,
-    }
+    },
+    plugins: [
+        viteStaticCopy({
+            targets: [{
+                src: 'node_modules/@shoelace-style/shoelace/dist/assets/*',
+                dest: 'shoelace',
+            }]
+        })
+    ],
 })
