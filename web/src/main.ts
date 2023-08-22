f62f4a6a333b0fa1b36323255e0a2c824bf6ff0e
Yasser Greyeb <ygreyeb@gmail.com>
2023-08-22 17:58:51 -0500

    Add currency formatter

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,3 @@
 import 'htmx.org'
 import './style.css'
-import {setBasePath} from '@shoelace-style/shoelace/dist/utilities/base-path.js'
-import '@shoelace-style/shoelace/dist/themes/light.css'
-import '@shoelace-style/shoelace/dist/components/format-date/format-date.js'
-import '@shoelace-style/shoelace/dist/components/spinner/spinner.js'
-
-setBasePath('/shoelace')
+import './script.d/shoelace.config.ts'
---
+++ web/src/script.d/shoelace.config.ts
@@ -0,0 +1,8 @@
+import {setBasePath} from '@shoelace-style/shoelace/dist/utilities/base-path.js'
+
+import '@shoelace-style/shoelace/dist/themes/light.css'
+import '@shoelace-style/shoelace/dist/components/format-date/format-date.js'
+import '@shoelace-style/shoelace/dist/components/format-number/format-number.js'
+import '@shoelace-style/shoelace/dist/components/spinner/spinner.js'
+
+setBasePath('/shoelace')
