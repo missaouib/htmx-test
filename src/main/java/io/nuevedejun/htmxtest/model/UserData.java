bccf48274a91bc93c9b19de51fe9bb443fc3c6b2
Yasser Greyeb <ygreyeb@gmail.com>
2023-09-02 23:31:40 -0500

    Fix error of double count query

--- src/main/java/io/nuevedejun/htmxtest/model/UserData.java
+++ src/main/java/io/nuevedejun/htmxtest/model/UserData.java
@@ -4,18 +4,20 @@ import io.nuevedejun.htmxtest.dto.TransactionData;
 import io.nuevedejun.htmxtest.entity.UserInfo;
 import io.nuevedejun.htmxtest.entity.UserInfo.Preferences;
 import io.nuevedejun.htmxtest.service.TransactionService;
-import io.nuevedejun.htmxtest.service.TransactionService.PageItem;
 import io.nuevedejun.htmxtest.service.UserInfoService;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.lang.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Provide accessors for relevant user information to be used in web templates.
  */
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserData {
 	public static final int FIRST_PAGE = 1;
 	public static final int FALLBACK_SIZE = ModelData.PAGE_SIZES.first();
@@ -32,12 +34,72 @@ public class UserData {
 	@Nullable
 	private Integer size;
 
+	@Nullable
+	private Page<TransactionData> transactionPage;
+	@Nullable
+	private List<TransactionData> transactions;
+	@Nullable
+	private List<PageItem> pagination;
+
+	public UserData(TransactionService transactionService, UserInfoService userInfoService, int page, @Nullable Integer size) {
+		this(transactionService, userInfoService, page);
+		this.size = size;
+	}
+
+	private Page<TransactionData> getTransactionPage() {
+		if (transactionPage == null) {
+			transactionPage = transactionService.getLatestTransactions(page - 1, getSize());
+		}
+		return transactionPage;
+	}
+
 	public List<TransactionData> getTransactions() {
-		return transactionService.getLatestTransactions(page - 1, getSize());
+		if (transactions == null) {
+			transactions = getTransactionPage().toList();
+		}
+		return transactions;
+	}
+
+	public record PageItem(int number, boolean selected, boolean blank) {
+		public static final PageItem GAP = new PageItem(-1, false, true);
 	}
 
 	public List<PageItem> getPagination() {
-		return transactionService.getPagination(page, getSize(), FALLBACK_WINDOW);
+		if (pagination == null) {
+			pagination = buildPagination();
+		}
+		return pagination;
+	}
+
+	private List<PageItem> buildPagination() {
+		final int totalPages = getTransactionPage().getTotalPages();
+
+		final int slider = 2 * FALLBACK_WINDOW + 1;
+		final int lower = Math.max(Math.min(page - FALLBACK_WINDOW, totalPages - 1 - slider), 2);
+		final int upper = Math.min(Math.max(page + FALLBACK_WINDOW, 2 + slider), totalPages - 1);
+
+		var result = new ArrayList<PageItem>(4 + slider);
+		result.add(selectedPageItem(page, 1));
+		if (lower == 3) {
+			result.add(selectedPageItem(page, 2));
+		} else if (lower > 3) {
+			result.add(PageItem.GAP);
+		}
+		result.addAll(IntStream.rangeClosed(lower, upper)
+				.mapToObj(i -> selectedPageItem(page, i))
+				.toList());
+		if (upper == totalPages - 2) {
+			result.add(selectedPageItem(page, totalPages - 1));
+		} else if (upper < totalPages - 2) {
+			result.add(PageItem.GAP);
+		}
+		result.add(selectedPageItem(page, totalPages));
+
+		return List.copyOf(result);
+	}
+
+	private PageItem selectedPageItem(int page, int item) {
+		return new PageItem(item, page == item, false);
 	}
 
 	public int getSize() {
--- src/main/java/io/nuevedejun/htmxtest/service/TransactionService.java
+++ src/main/java/io/nuevedejun/htmxtest/service/TransactionService.java
@@ -4,23 +4,19 @@ import io.nuevedejun.htmxtest.dto.TransactionData;
 import io.nuevedejun.htmxtest.entity.Transaction;
 import io.nuevedejun.htmxtest.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
 	private final TransactionRepository repository;
 
-	public List<TransactionData> getLatestTransactions(int page, int size) {
+	public Page<TransactionData> getLatestTransactions(int page, int size) {
 		var pageable = PageRequest.of(page, size, Sort.Direction.DESC, "date");
-		return repository.findAll(pageable).stream()
-				.map(this::toDTO).toList();
+		return repository.findAll(pageable).map(this::toDTO);
 	}
 
 	private TransactionData toDTO(Transaction transaction) {
@@ -31,49 +27,5 @@ public class TransactionService {
 				transaction.getNote(),
 				transaction.getAmount()
 		);
-	}
-
-	public record PageItem(int number, boolean selected, boolean blank) {
-		public static final PageItem GAP = new PageItem(-1, false, true);
-	}
-
-	/**
-	 * Return a list of pages to show in the pagination of transactions.
-	 *
-	 * @param page   the currently selected page
-	 * @param size   the page size
-	 * @param window the window size around the selected page
-	 * @return the list of pages
-	 */
-	public List<PageItem> getPagination(int page, int size, int window) {
-		final long count = repository.count();
-		final int allPages = (int) Math.ceilDiv(count, size);
-
-		final int slider = 2 * window + 1;
-		final int lower = Math.max(Math.min(page - window, allPages - 1 - slider), 2);
-		final int upper = Math.min(Math.max(page + window, 2 + slider), allPages - 1);
-
-		var result = new ArrayList<PageItem>(4 + slider);
-		result.add(selectedPageItem(page, 1));
-		if (lower == 3) {
-			result.add(selectedPageItem(page, 2));
-		} else if (lower > 3) {
-			result.add(PageItem.GAP);
-		}
-		result.addAll(IntStream.rangeClosed(lower, upper)
-				.mapToObj(i -> selectedPageItem(page, i))
-				.toList());
-		if (upper == allPages - 2) {
-			result.add(selectedPageItem(page, allPages - 1));
-		} else if (upper < allPages - 2) {
-			result.add(PageItem.GAP);
-		}
-		result.add(selectedPageItem(page, allPages));
-
-		return List.copyOf(result);
-	}
-
-	private PageItem selectedPageItem(int page, int item) {
-		return new PageItem(item, page == item, false);
 	}
 }
